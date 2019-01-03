d7df8ee3880edcf61ef20771177a292ef48ce265
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-01-04 09:58:20 +1100

    add app and listen

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ const bodyParser = require('body-parser');
 // simplifies file paths
     // core module, so doesn't need to be npm installed
 const path = require('path');
+
+const app = express();
+
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
