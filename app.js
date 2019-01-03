8686978c80b878e0f6b31fbaf71c92e84a109af5
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-01-04 10:23:02 +1100

    add example middleware

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ const path = require('path');
 
 const app = express();
 
+/* EXAMPLE OF MIDDLEWARE
+// custom middleware
+const logger = ((req, res, next) => {
+    console.log('logging...');
+    next();
+})
+
+// allows us to use the custom middleware
+app.use(logger);
+*/
+
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 
 // process.env.PORT lets the port be set by Heroku
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3033;
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
 });
