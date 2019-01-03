55a43067d141935e6a4ff1bf6ce97766b02340eb
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-01-04 09:47:42 +1100

    require modules

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// simplifies file paths
+    // core module, so doesn't need to be npm installed
+const path = require('path');
